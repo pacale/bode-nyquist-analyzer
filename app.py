753d65ec28ec3b001fc5432d674dcab19e1612cc
pacale <pacale@users.noreply.github.com>
2026-03-28 21:58:22 +0100

    fix: remove unused pandas import and explicit scipy dependency for streamlit cloud

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ from typing import Optional
 
 import control as ctrl  # type: ignore
 import numpy as np
-import pandas as pd
 import plotly.graph_objects as go  # type: ignore
 import streamlit as st
 import sympy
